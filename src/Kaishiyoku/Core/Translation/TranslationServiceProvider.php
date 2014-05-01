882ea1678e23a9f17aa74277924ca9b0be64d3be
Kaishiyoku <Kaishiyoku@users.noreply.github.com>
2014-05-02 00:14:51 +0200

    Update TranslationServiceProvider.php

--- src/Kaishiyoku/Core/Translation/TranslationServiceProvider.php
+++ src/Kaishiyoku/Core/Translation/TranslationServiceProvider.php
@@ -6,7 +6,7 @@ class TranslationServiceProvider extends IlluminateTranslationServiceProvider
 {
 	/**
 	 * Register the translation line loader.
-     	* Add support for YAML files.
+     	 * Add support for YAML files.
 	 *
 	 * @return void
 	 */
